2f2b8e183e9e2a3eb1e74483203a65ddcc8aff32
Jens Mauersberger <j.mauersberger@stratec.com>
2023-12-05 15:23:07 +0100

    Bugfix Day5

--- 2023/Day5.cs
+++ 2023/Day5.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace AoC2023
 {
+  class Seeds
+  {
+    private List<long> RangeStart { get; set; } = new List<long>();
+    private List<long> Length { get; set; } = new List<long>();
+
+    public void AddRanges(List<long> numbers)
+    {
+      for (int idx = 0; idx < numbers.Count / 2; idx++)
+      {
+        RangeStart.Add(numbers[2*idx]);
+        Length.Add(numbers[2*idx + 1]);
+      }
+    }
+
+    public bool ContainsSeed(long seed)
+    {
+      for (int idx = 0; idx < RangeStart.Count; idx++)
+      {
+        var startIdx = RangeStart[idx];
+        if (startIdx <= seed && seed < startIdx + Length[idx])
+          return true;
+      }
+
+      return false;
+    }
+  }
+
   class Map
   {
     private List<long> DestRangeStart { get; set; } = new List<long>();
@@ -30,17 +56,52 @@ namespace AoC2023
       }
       return src;
     }
+
+    public IEnumerable<long> GetLowestDestinations()
+    {
+      var destDict = new Dictionary<long, int>();
+      for (int idx =0; idx<DestRangeStart.Count(); idx++)
+      {
+        destDict.Add(DestRangeStart[idx], idx);
+      }
+
+      var tmpRange = new List<long>();
+      tmpRange.AddRange(DestRangeStart);
+      tmpRange.Sort();
+      for (int idx = 0; idx < tmpRange.Count; idx++)
+      {
+        var startIdx = tmpRange[idx];
+        for (var idx2 = startIdx; idx2 < startIdx + Length[destDict[startIdx]]; idx2++)
+        {
+          yield return idx2;
+        }
+      }
+    }
+
+    internal long Dest2Src(long dest)
+    {
+      for (int idx = 0; idx < DestRangeStart.Count; idx++)
+      {
+        var destination = DestRangeStart[idx];
+        if (dest >= destination && dest < destination + Length[idx])
+        {
+          return dest - destination + SourceRangeStart[idx];
+        }
+      }
+
+      return dest;
+    }
   }
+  
   internal class Day5 : ISolver
   {
-    List<long> seeds = new List<long>();
-    Map seed2Soil = new Map();
-    Map soil2Fertilizer = new Map();
-    Map fertilizer2Water = new Map();
-    Map water2Light = new Map();
-    Map light2Temperature = new Map();
-    Map temperature2Humidity = new Map();
-    Map humidity2Location = new Map();
+    readonly Map seed2Soil = new Map();
+    readonly Map soil2Fertilizer = new Map();
+    readonly Map fertilizer2Water = new Map();
+    readonly Map water2Light = new Map();
+    readonly Map light2Temperature = new Map();
+    readonly Map temperature2Humidity = new Map();
+    readonly Map humidity2Location = new Map();
     
     public void ReadInput(string[] allLines)
     {
@@ -82,47 +143,66 @@ namespace AoC2023
 
     public void Solve1(string[] allLines)
     {
-      seeds = ReadNumbers(allLines[0].Split(':')[1].Trim());
+      var seeds = ReadNumbers(allLines[0].Split(':')[1].Trim());
       ReadInput(allLines);
-      List<long> locations = SeedToLocations();
+      var minLocation = FindMinLocation(seeds);
 
-      Console.WriteLine($"Task1: {locations.Min()}");
-      return;
+      Console.WriteLine($"Task1: {minLocation}");
     }
 
-    private List<long> SeedToLocations()
+    private long FindMinLocation(IEnumerable<long> seeds)
     {
-      List<long> locations = new List<long>();
+      long minLocation = long.MaxValue;
       foreach (var seed in seeds)
       {
-        locations.Add(humidity2Location.Src2Dest(
+        var loc = humidity2Location.Src2Dest(
           temperature2Humidity.Src2Dest(
             light2Temperature.Src2Dest(
               water2Light.Src2Dest(
                 fertilizer2Water.Src2Dest(
                   soil2Fertilizer.Src2Dest(
-                    seed2Soil.Src2Dest(seed))))))));
+                    seed2Soil.Src2Dest(seed)))))));
+
+        minLocation = Math.Min(minLocation, loc);
       }
-      return locations;
+      return minLocation;
+    }
+
+    private long FindMinLocationReverse(Seeds seeds)
+    {
+      long minLocation = long.MaxValue;
+      int noOfTries = 1;
+      foreach (var soil in humidity2Location.GetLowestDestinations())
+      {
+        var seed = seed2Soil.Dest2Src(
+          soil2Fertilizer.Dest2Src(
+            fertilizer2Water.Dest2Src(
+              water2Light.Dest2Src(
+                light2Temperature.Dest2Src(
+                  temperature2Humidity.Dest2Src(
+                    humidity2Location.Dest2Src(soil)))))));
+        if (seed > 0 && seeds.ContainsSeed(seed))
+        {
+          minLocation = Math.Min(minLocation, soil);
+          Console.WriteLine($"{seed} - {soil} after {noOfTries} tries.");
+          break;
+        }
+
+        noOfTries++;
+      }
+
+      return minLocation;
     }
 
     public void Solve2(string[] allLines)
     {
       var seedInput = ReadNumbers(allLines[0].Split(':')[1].Trim());
-      seeds = new List<long>();
-      for (int idx=0; idx<seedInput.Count/2; idx++)
-      {
-        var startIdx = seedInput[2*idx];
-        for (var idx2 = startIdx; idx2 < startIdx + seedInput[2 * idx + 1]; idx2++)
-        {
-          seeds.Add(idx2);
-        }
-      }
-      ReadInput(allLines);
-      List<long> locations = SeedToLocations();
+      var seeds = new Seeds();
+      seeds.AddRanges(seedInput);
 
-      Console.WriteLine($"Task2: {locations.Min()}");
-      return;
+      long minLocation = FindMinLocationReverse(seeds);
+
+      Console.WriteLine($"Task2: {minLocation}");
     }
   }
 }
