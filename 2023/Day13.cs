6fa009dc948b6f280374cdc79cf243146a6e3d8f
JMaue <j.mauersberger@freenet.de>
2023-12-13 21:03:40 +0100

    Day13 Part2

--- 2023/Day13.cs
+++ 2023/Day13.cs
@@ -9,21 +9,21 @@ namespace AoC2023
 {
   internal class Day13 : ISolver
   {
-    private int FindHorizontalSymmetry(List<string> group)
+    private int FindHorizontalSymmetry(List<string> group, Func<string, string, bool> areIdentical)
     {
       for (int i = 0; i < group.Count; i++)
       {
         var line = group[i];
         if (i < group.Count - 1)
         {
-          if (line == group[i + 1])
+          if (areIdentical(line, group[i + 1]))
           {
             // walk upwards to see if all match
             var distance = Math.Min(i, group.Count - i - 2);
             bool isSymmetric = true;
             while (isSymmetric && distance >= 0)
             {
-              isSymmetric = group[i - distance] == group[i + distance + 1];
+              isSymmetric = areIdentical(group[i - distance], group[i + distance + 1]);
               distance--;
             }
             if (isSymmetric)
@@ -34,14 +34,40 @@ namespace AoC2023
       return 0;
     }
 
-    public int ProcessGroup (List<string> group)
+    public int ProcessGroup1 (List<string> group)
     {
-      var rc = 100*FindHorizontalSymmetry(group);
-      rc += FindVerticalSymmetry(group);
+      bool areIdentical(string s1, string s2) => s1 == s2;
+
+      var rc = 100 * FindHorizontalSymmetry(group, areIdentical);
+      rc += FindVerticalSymmetry(group, areIdentical);
+      Console.WriteLine(rc);
       return rc;
     }
 
-    private int FindVerticalSymmetry(List<string> group)
+    public int ProcessGroup2(List<string> group)
+    {
+      bool areIdentical(string s1, string s2)
+      {
+        if (s1 == s2)
+          return true;
+
+        int cnt = 0;
+        for (int i = 0; i < s1.Length; i++)
+        {
+          if (s1[i] != s2[i])
+            cnt++;
+        }
+
+        return cnt == 1;
+      }
+
+      var rc = 100 * FindHorizontalSymmetry(group, areIdentical);
+      rc += FindVerticalSymmetry(group, areIdentical);
+      Console.WriteLine(rc);
+      return rc;
+    }
+
+    private int FindVerticalSymmetry(List<string> group, Func<string, string, bool> areIdentical)
     {
       var rotatedGroup = new List<string>();
       bool first = true;
@@ -63,7 +89,7 @@ namespace AoC2023
           }
         }
       }
-      return FindHorizontalSymmetry(rotatedGroup);
+      return FindHorizontalSymmetry(rotatedGroup, areIdentical);
     }
 
     public void Solve1(string[] allLines)
@@ -74,7 +100,7 @@ namespace AoC2023
       {
         if (line.Length == 0 || line == "END")
         {
-          sum += ProcessGroup(group);
+          sum += ProcessGroup1(group);
           group.Clear();
         }
         else
@@ -87,7 +113,21 @@ namespace AoC2023
 
     public void Solve2(string[] allLines)
     {
-      Console.WriteLine($"Task2: {0+0}");
+      var group = new List<string>();
+      var sum = 0;
+      foreach (var line in allLines)
+      {
+        if (line.Length == 0 || line == "END")
+        {
+          sum += ProcessGroup2(group);
+          group.Clear();
+        }
+        else
+        {
+          group.Add(line);
+        }
+      }
+      Console.WriteLine($"Task2: {sum}");
     }
   }
 }
